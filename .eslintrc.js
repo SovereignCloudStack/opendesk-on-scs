d0a4c5cfcc0729cacdcb30c71834bb33b1b5815a
Tim Breidenbach <breidenbach@univention.de>
2024-10-01 11:37:19 +0200

    feat: add pre-commit configs from docs.scs.community
      https://docs.scs.community/community/contribute/linting-guide

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['plugin:react/recommended', 'standard-with-typescript'],
+  overrides: [
+    {
+      files: 'docs/*.md',
+      parser: 'eslint-plugin-markdownlint/parser',
+      extends: 'plugin:markdownlint/recommended'
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react'],
+  rules: [
+    {
+      'space-before-function-paren': 'never'
+    }
+  ]
+}
